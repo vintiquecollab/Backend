beb0852dd2d3d8a4f998e4ca0230ab3d5ebf83b0
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-03-25 12:36:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("./config/db");
 const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
-const userRoutes = require("./routes/userRoutes");
+// const userRoutes = require("./routes/userRoutes");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
