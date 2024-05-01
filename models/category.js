35272fdd8cc6131089170bf56df19f877f6fd050
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-05-02 00:21:03 +0100

    updates

--- models/category.js
+++ models/category.js
@@ -1,31 +1,31 @@
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   media: {
     type: String,
-    required:true
+    required: false,
   },
   status: {
     type: Boolean,
-    default: true
+    default: true,
   },
 
   createdAt: {
     type: Date,
     default: Date.now,
-    required: true
+    required: true,
   },
   updatedAt: {
     type: Date,
     default: Date.now,
-    required: true
-  }
+    required: true,
+  },
 });
 
-const Category = mongoose.model('Category', categorySchema);
+const Category = mongoose.model("Category", categorySchema);
 
 module.exports = Category;
